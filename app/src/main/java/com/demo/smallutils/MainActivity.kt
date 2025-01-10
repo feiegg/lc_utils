ecd752e954e5426b305c54dfff7127c6f88f7e21
tanhonggwu <tom@feiegg.com>
2025-01-10 12:43:25 +0800

    1.0.1

--- app/src/main/java/com/demo/smallutils/MainActivity.kt
+++ app/src/main/java/com/demo/smallutils/MainActivity.kt
@@ -6,14 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
-import com.galaxyeye.fcsdk.FCAct
-import com.galaxyeye.fcsdk.FCFlutterDataCallback
-import com.galaxyeye.fcsdk.FCFlutterFuncEn
-import com.galaxyeye.fcsdk.FCFlutterMethod
-import com.galaxyeye.fcsdk.FCNativeMethod
-import com.galaxyeye.fcsdk.FlutterFuncHandle
+import com.smallcake.utils.SdUtils
+//import com.galaxyeye.fcsdk.FCAct
+//import com.galaxyeye.fcsdk.FCFlutterDataCallback
+//import com.galaxyeye.fcsdk.FCFlutterFuncEn
+//import com.galaxyeye.fcsdk.FCFlutterMethod
+//import com.galaxyeye.fcsdk.FCNativeMethod
+//import com.galaxyeye.fcsdk.FlutterFuncHandle
 import com.smallcake.utils.buildCDTimer
 import com.smallcake.utils.buildSpannableString
+import com.smallcake.utils.ToastUtil
 import kotlin.math.absoluteValue
 import kotlin.random.Random
 
@@ -31,70 +33,71 @@ class MainActivity : AppCompatActivity() {
                 isBold = true
                 textColor = Color.RED
                 onClick{
-                    startFACTActivity()
+//                    startFACTActivity()
+                    ToastUtil.showShort("你好呀")
                 }
             }
         }
         speedChartView.setProgress(80f)
     }
-    fun  startFACTActivity(){
-        //注册原生方法回调
-        FlutterFuncHandle.registerDataCallback(object : FCFlutterDataCallback {
-            override fun onData(
-                method: String,
-                data: Any?,
-                success: (data: Any?) -> Unit,
-                fail: (String?) -> Unit
-            ) {
-                when (method) {
-                    FCNativeMethod.connectBluetooth.name -> {
-                        Log.d("connectBluetooth", data.toString())
-                        success(emptyMap<String, Any>())
-                    }
-
-                    FCNativeMethod.receiveDirective.name -> {
-                        val directiveData = data as Map<String, Any?>
-                        Log.d("receiveDirective", data.toString())
-                        //指令类型
-                        val directiveType = directiveData["directiveType"] as String
-                        //指令
-                        val directive = directiveData["directive"] as String
-                        success(emptyMap<String, Any>())
-                    }
-
-                    FCNativeMethod.configureAudioSession.name -> {
-                        Log.d("configureAudioSession", data.toString())
-                        success(emptyMap<String, Any>())
-                    }
-
-                    else -> {
-                        Log.d("onData", "$method $data")
-                        success(emptyMap<String, Any>())
-                    }
-                }
-            }
-
-            override fun onMethodRegistered(channelMap: Map<String, FCFlutterMethod>) {
-                //接收注册成功的Flutter回调
-                Log.d("onMethodRegistered", channelMap.toString())
-            }
-
-            override fun onLoaded() {
-                val result = FlutterFuncHandle.callFlutterMethod(
-                    FCFlutterFuncEn.setupUserAndDevice, mapOf<String, Any?>(
-                        "uid" to Random(System.currentTimeMillis()).nextInt().absoluteValue.toString(),
-                        "terminalId" to "bma1070400048363732992",
-                        "wxAppId" to "xxx"
-                    )
-                )
-                FlutterFuncHandle.callFlutterMethod(
-                    FCFlutterFuncEn.bluetoothNotification, mapOf<String, Any?>("type" to "1")
-                )
-            }
-        })
-
-        //启动SDK界面
-        val i = Intent(this, FCAct::class.java)
-        startActivity(i)
-    }
+//    fun  startFACTActivity(){
+//        //注册原生方法回调
+//        FlutterFuncHandle.registerDataCallback(object : FCFlutterDataCallback {
+//            override fun onData(
+//                method: String,
+//                data: Any?,
+//                success: (data: Any?) -> Unit,
+//                fail: (String?) -> Unit
+//            ) {
+//                when (method) {
+//                    FCNativeMethod.connectBluetooth.name -> {
+//                        Log.d("connectBluetooth", data.toString())
+//                        success(emptyMap<String, Any>())
+//                    }
+//
+//                    FCNativeMethod.receiveDirective.name -> {
+//                        val directiveData = data as Map<String, Any?>
+//                        Log.d("receiveDirective", data.toString())
+//                        //指令类型
+//                        val directiveType = directiveData["directiveType"] as String
+//                        //指令
+//                        val directive = directiveData["directive"] as String
+//                        success(emptyMap<String, Any>())
+//                    }
+//
+//                    FCNativeMethod.configureAudioSession.name -> {
+//                        Log.d("configureAudioSession", data.toString())
+//                        success(emptyMap<String, Any>())
+//                    }
+//
+//                    else -> {
+//                        Log.d("onData", "$method $data")
+//                        success(emptyMap<String, Any>())
+//                    }
+//                }
+//            }
+//
+//            override fun onMethodRegistered(channelMap: Map<String, FCFlutterMethod>) {
+//                //接收注册成功的Flutter回调
+//                Log.d("onMethodRegistered", channelMap.toString())
+//            }
+//
+//            override fun onLoaded() {
+//                val result = FlutterFuncHandle.callFlutterMethod(
+//                    FCFlutterFuncEn.setupUserAndDevice, mapOf<String, Any?>(
+//                        "uid" to Random(System.currentTimeMillis()).nextInt().absoluteValue.toString(),
+//                        "terminalId" to "bma1070400048363732992",
+//                        "wxAppId" to "xxx"
+//                    )
+//                )
+//                FlutterFuncHandle.callFlutterMethod(
+//                    FCFlutterFuncEn.bluetoothNotification, mapOf<String, Any?>("type" to "1")
+//                )
+//            }
+//        })
+//
+//        //启动SDK界面
+//        val i = Intent(this, FCAct::class.java)
+//        startActivity(i)
+//    }
 }
